2b3e63dea02573db88f967c5630b3e6a97148ec0
arg387 <84933329+arg387@users.noreply.github.com>
2024-03-02 17:46:12 +0530

    3rd commit

---
+++ first/index.js
@@ -0,0 +1,8 @@
+console.log('Hello');
+console.log('what are you doing?');
+
+window.alert('Are you good to go?');
+//THis is a comment
+/* THis is a comment */
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `THis is it`;
---
+++ variablesandarithmeticoperators/index.js
@@ -0,0 +1,46 @@
+// ⁡⁣⁢⁢variables = A container that stores a value.⁡
+//⁡⁣⁣⁢behaves as if it were the value it contains.⁡
+
+/* let age = 25;
+let price = 10.99;
+let fvfood = "pizza";
+console.log(typeof price);
+console.log(`You are ${age} years old`);
+console.log(`The price is $${price}.`);
+console.log(`I like ${fvfood}`);
+console.log(typeof fvfood); */
+
+// ⁡⁢⁢⁣booleans are either true or false.⁡
+
+/* let online = true;
+let fullName = "Argha Saha";
+let age = 22;
+let isStudent = true;
+
+
+console.log(`I am online.: ${online}`);
+document.getElementById("idh1").textContent = `My name is ${fullName}`;
+document.getElementById("idh2").textContent = age;
+document.getElementById("idh3").textContent = `Am i a Student: ${isStudent} i am` ; */
+
+/* ⁡⁣⁣⁢------------------Arithmetic operator------------------------------------------------------⁡ */
+
+//⁡⁣⁢⁣arithmetic operator = operands (values, variables etc.);⁡
+//                     example: x+5 = 16;
+
+let students = 30;
+/* let student = 31; */
+/* student = student - 30;
+student = student + 30; */
+/* student = student * 3;
+student = student / 3; */
+/* student = student ** 3; */
+/* let extraStudent = student % 3; */
+/* console.log(extraStudent); */
+
+// ⁡⁢⁣⁣augmented assigned operators ⁡⁢⁣⁣examples⁡
+// example: += , -= , *= , /= , **= , %=;
+students += 1;
+
+console.log(students);
+

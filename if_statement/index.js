f6df5825175d36abb4e97e9c2b26d09099175373
arg387 <84933329+arg387@users.noreply.github.com>
2024-03-07 12:26:18 +0530

    5th commit

---
+++ if_statement/index.js
@@ -0,0 +1,39 @@
+// ⁡⁣⁣⁢IF statements : if a condition is true then execute the statement or else not..⁡
+
+/* let age;
+age = window.prompt("What is your age");
+
+if (age >= 18) {
+  window.alert("You are eligible to visit this site, please enter...");
+}
+
+else if (age < 18) {
+/ ⁡⁣⁣⁢here while is used to make a infinite loop. ⁡
+  while(true) {
+    window.alert("You are not eligible to visit this site, please leave...");
+  }
+
+} */
+
+const myText = document.getElementById("myText");
+const myButton = document.getElementById("myButton");
+const resultElement = document.getElementById("resultElement");
+let age;
+myButton.onClick = function(){
+  age = Number(myText.value);
+    if (age >= 100) {
+      resultElement.textContent =`You are too old to enter this site.`;
+    }
+    else if (age == 0) {
+      resultElement.textContent =`You are too young to enter this site.`;
+    }
+    else if (age >= 18) {
+      resultElement.textContent =`You can enter this site now.`;
+    }
+    else if (age < 0) {
+      resultElement.textContent =`You are not a human.`;
+    }
+    else {
+      resultElement.textContent =`You can only this site enter when your age crosses 18.`;
+    }
+}
--- math/index.js
+++ math/index.js
@@ -1,11 +1,12 @@
 // ⁡⁢⁣⁢Math = build in objects that provides a collection of properties and methods.⁡
 
-let x = 4;
+/* let x = 1.555;
 let y = 5;
-let z = 8;
+let z = 8; */
 /* z= Math.round(x); */
+// this will return values like if x= 1.54 then x= 2
 /* z= Math.floor(x); */
-// ⁡⁣⁣⁡⁣⁣⁢same work is done by both round and floor⁡
+// ⁡⁣⁣⁢somehow same work is done by both round and floor⁡⁡
 /* z= Math.ceil(x);  */
 // ⁡⁣⁣⁢opposite work is done by ceil⁡
 /* z= Math.trunc(x);  */
@@ -29,9 +30,36 @@ let z = 8;
 // ⁡⁣⁣⁡⁣⁣⁢���������������return the absolute of the given number.��⁡
 /* z= Math.sign(x); */
 
+/* console.log(z); */
 // ⁡⁣⁣⁢������������������������������return the sign of the given number.����⁡
 // ⁡⁣⁣⁢note: among the two js command last one will work.⁡
 /* let max = Math.max(x,y,z);
 let min = Math.min(x,y,z); */
 
-console.log(max,min);
+/* console.log(min,max); */
+
+
+//⁡⁢⁣⁢Random number generator⁡
+
+const myButton = document.getElementById("myButton");
+const label1 = document.getElementById("label1");
+const label2 = document.getElementById("label2");
+const label3 = document.getElementById("label3");
+const min = 1;
+const max = 6;
+
+let randomNum1 =0;
+let randomNum2 =0;
+let randomNum3 =0;
+
+
+myButton.onclick = function(){
+  randomNum1 = Math.floor(Math.random() * max) + min;
+  randomNum2 = Math.floor(Math.random() * max) + min;
+  randomNum3 = Math.floor(Math.random() * max) + min;
+  label1.textContent = randomNum1;
+  label2.textContent = randomNum2;
+  label3.textContent = randomNum3;
+}
+
+// For Math.random() method -- Expected output: a number from 0 to <1

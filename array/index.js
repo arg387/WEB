ae181e5cc353af9803f2cb210fec1344ff7bea5a
arg387 <84933329+arg387@users.noreply.github.com>
2024-04-22 01:33:38 +0530

    8th commit

--- array/index.js
+++ array/index.js
@@ -1,1 +1,34 @@
-// array = a variable
+// ⁡⁣⁣⁢array = a variable like structure that can hold multiple values⁡
+let fruits = ["apple", "orange","pineapple"];
+fruits[0] = "pear";
+
+// ⁡⁣⁢⁣The push() method is used to add one or more elements to the end of an array⁡
+/* fruits.push("mango"); */
+// ⁡⁣⁣⁡⁣⁢⁣The pop() method is used to remove the last element from an array⁡
+/* fruits.pop(); */
+// ⁡⁣⁢⁣The unshift() method is used to add one or more elements to the beginning of an array.⁡
+/* fruits.unshift("berry"); */
+// ⁡⁣⁣⁡⁣⁢⁣The shift() method is used to remove the first element from an array.⁡
+/* fruits.shift(); */
+
+
+/* console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits[3]); */
+
+////////////////////////////////////////////
+
+/* let numOfFruits = fruits.length;
+let index = fruits.indexOf("pear");
+console.log(numOfFruits);
+console.log(index); */
+
+/* for (let i = 0; i < fruits.length; i++){
+  console.log(fruits[i]);
+  
+} */
+fruits.sort().reverse();
+for (let fruit of fruits){
+  console. log(fruit);
+}

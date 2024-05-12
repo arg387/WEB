fe9989ae0d768f12007925fc7d83c4b729a4c7e0
arg387 <84933329+arg387@users.noreply.github.com>
2024-05-12 20:00:59 +0530

    14th commit

--- array/destructuring/index.js
+++ array/destructuring/index.js
@@ -3,11 +3,31 @@
 // {} = to perform object destructuring.
 
 // -------------- example 1 -------------
-// swap the value of two variables
+// swap the value of two variables.
 
 let a = 1 ;
 let b = 2 ;
 
 [a, b] = [b , a];
+
 console.log(a);
 console.log(b);
+
+// ------------- example 2 --------------
+// swap the elements in an array.
+
+let arr = [1, 2, 3, 4, 5] ;
+
+[arr[0],arr[1],arr[2],arr[3],arr[4]] = [arr[1],arr[0],arr[2],arr[4],arr[3]];
+console.log(arr); 
+
+// ---------------- example 3 ------------------
+// assign values to the variables 
+
+let colors = ["red", "green", "yellow","orange","dark red","purple"];
+
+let [color1, color2, color3, ...extraColors] = colors;
+console.log(color1);
+console.log(color2);
+console.log(color3);
+console.log(extraColors);
--- reset_parameters/index.js
+++ reset_parameters/index.js
@@ -5,18 +5,18 @@
   console.log(foods);
 }
 
-const food1 = "pizza";
 const food2 = "hamburger";
 const food3 = "hotdog";
 const food4 = "fried rice";
 const food5 = "rice ball";
 
-openFridge(food1, food2, food3, food4, food5); */
+openFridge("pizza", food2, food3, food4, food5); */
 
-
+// look closely since this works 
 ///////////////////////////////
-
-/* function sum (...numbers){
+// look closely since both this works 
+let total = 0;
+function sum (...numbers){
   let total = 0;
   for (let number of numbers ){
     total += number;
@@ -24,7 +24,7 @@ openFridge(food1, food2, food3, food4, food5); */
   return total;
 }
 function avg (...numbers){
-let total = 0;
+  let total = 0;
 let count = numbers.length;  
   for (let number of numbers ){
     total += number;
@@ -32,15 +32,36 @@ let count = numbers.length;
   return total/count;
 }
 console.log(sum(1,2,5));
-console.log(avg(1,2,5)); */
+console.log(avg(1,2,5));
+// you cannot declare it here ``` let total = 0;``` because if you do that you are declaring the variable after function call. 
+
+///////////
+
+function sum (...numbers){
+
+  for (let number of numbers ){
+    total += number;
+  }
+  return total;
+}
+function avg (...numbers){
+
+let count = numbers.length;  
+  for (let number of numbers ){
+    total += number;
+  }
+  return total/count;
+}
+console.log(sum(10,20,50));
+console.log(avg(10,20,50));
 
 ////////////////////////////////
 // ⁡⁣⁢⁢combine strings⁡
 
-function combineStrings(...string){
+/* function combineStrings(...string){
   return string.join(" ");
 
 }
 
 const result = combineStrings("Mr","John", "Beckman");
-console.log(result);
+console.log(result); */
